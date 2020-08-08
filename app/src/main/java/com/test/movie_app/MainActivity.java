ec6856c366e3df588bd9cba16957d1d88b43548f
Discoonect <67423146+Discoonect@users.noreply.github.com>
2020-08-08 15:51:37 +0900

    a

--- app/src/main/java/com/test/movie_app/MainActivity.java
+++ app/src/main/java/com/test/movie_app/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -37,17 +38,66 @@ public class MainActivity extends AppCompatActivity {
     int offset;
     JSONObject jsonObject = new JSONObject();
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edit_search = findViewById(R.id.edit_search);
+        img_search = findViewById(R.id.img_search);
+
+
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
-        Log.i("aaa",""+offset);
+        img_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String searchKeyword = edit_search.getText().toString().trim();
 
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    Util.BASE_URL + "/api/v1/movie/search?q=" + searchKeyword + "&offset=" + offset, null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                JSONArray jsonArray = response.getJSONArray("movie");
+                                Log.i("aaa",jsonArray.toString());
+                                for(int i=0; i<jsonArray.length();i++){
+
+                                    int id = jsonObject.getInt("id");
+                                    String title = jsonObject.getString("title");
+                                    String genre = jsonObject.getString("genre");
+                                    int attendance = jsonObject.getInt("attendance");
+                                    String year = jsonObject.getString("year");
+
+                                    Movie movie = new Movie(id,title,genre,attendance,year);
+                                    list.add(movie);
+                                    Log.i("aaa","1");
+                                }
+                                recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this,list);
+                                recyclerView.setAdapter(recyclerViewAdapter);
+                                Log.i("aaa","2");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.i("aaa",error.toString());
+                        }
+                    }
+                );
+                Volley.newRequestQueue(MainActivity.this).add(jsonObjectRequest);
+                Log.i("aaa","3");
+            }
+        });
 
         JsonObjectRequest request = new JsonObjectRequest(
             Request.Method.GET,
@@ -73,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                         recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this,list);
                         recyclerView.setAdapter(recyclerViewAdapter);
+                        Log.i("aaa","4");
 
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -82,8 +133,6 @@ public class MainActivity extends AppCompatActivity {
             new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-
-
                 }
             }
         );
